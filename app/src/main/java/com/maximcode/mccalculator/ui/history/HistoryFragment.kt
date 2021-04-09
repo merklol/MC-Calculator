2a5a40ebad442b096585b183a9c7a91e243d18d4
maximsmolyakov <sm.maksim@icloud.com>
2021-04-09 23:26:00 +0800

    Adds a History screen
    This commit adds the history fragment and its layout to the project.

---
+++ app/src/main/java/com/maximcode/mccalculator/ui/history/HistoryFragment.kt
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Smolyakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.maximcode.mccalculator.ui.history
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.maximcode.mccalculator.R
+import com.maximcode.mccalculator.ui.history.historylist.HistoryAdapter
+import com.maximcode.mccalculator.databinding.FragmentHistoryBinding
+import com.maximcode.mccalculator.dto.HistoryState
+import com.maximcode.mccalculator.ui.history.historylist.decorations.HistoryMarginDecoration
+import com.maximcode.rxmvi.view.RxMviFragment
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class HistoryFragment
+@Inject constructor(private val adapter: HistoryAdapter)
+    : RxMviFragment<HistoryState, HistoryViewModel>(R.layout.fragment_history) {
+
+    override val viewModel: HistoryViewModel by viewModels()
+    private val binding: FragmentHistoryBinding by viewBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        viewModel.showHistory()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    override fun render(state: HistoryState) {
+        adapter.setData(state.history)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_history, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.actionClearHistory) {
+            viewModel.clearAllRecords()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupRecyclerView() {
+        binding.historyView.layoutManager = LinearLayoutManager(context)
+        binding.historyView.adapter = adapter
+        binding.historyView.addItemDecoration(
+            HistoryMarginDecoration(requireContext(), 16, 16)
+        )
+    }
+}
